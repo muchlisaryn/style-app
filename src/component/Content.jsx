75eb34002d09144c62335afee8e6ed929878a993
muchlisaryn <muchlisar68@gmail.com>
2022-09-13 21:54:23 +0700

    first commit

---
+++ src/component/Content.jsx
@@ -0,0 +1,11 @@
+export default function Content () {
+    return (
+        <div className="p-5 mb-4 bg-light rounded-3 border">
+            <div className="container-fluid py-5">
+                <h1>First Paragraph in Jumbotron</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum non voluptates sapiente, labore neque alias aperiam, vero fuga sunt excepturi libero deleniti. Officia perspiciatis neque, non in iste maxime. Vitae.</p>
+                <button className="btn btn-primary btn-lg">example button</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/component/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer () {
+    return (
+        <footer className="text-muted py-5 border-top">
+            <div className="container">
+            <p>This is My First React App in Bootstrap</p></div>
+            <p>New to React ? <a href="https://reactjs.org">visit the homepage</a></p>
+        </footer>
+    )
+}
